216e3694d7c8288dc4b4a407809ae251af023ade
Steve St.Pierre <stephencstpierre@gmail.com>
2020-08-14 13:11:11 -0400

    missed this file for token register

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
+const { createSessions } = require("./signin");
+
 const handleRegister = (db, bcrypt) => (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
+  console.log(`handleRegister`, req.body);
 
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
@@ -18,14 +21,21 @@ const handleRegister = (db, bcrypt) => (req, res) => {
             name,
             joined: new Date()
           })
-          .then(user => {
-            res.json(user[0])
+          .then(users => {
+            const user = users[0];
+
+            return user && user.id && user.email ? createSessions(user) : Promise.reject('User not created')
           })
+          .then(session => res.json(session))
+          .catch(err => Promise.reject(err))
       })
       .then(trx.commit)
       .catch(trx.rollback);
   })
-  .catch(error => res.status(400).json('Unable to register'));
+  .catch(error => {
+    console.log(`error registering`, error);
+    res.status(400).json('Unable to register')
+  });
 };
 
 module.exports = { handleRegister };
