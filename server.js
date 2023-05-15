50f42d8ccd2a224269f814d5d1e0a1dcf2d60446
Steve St.Pierre <stephencstpierre@gmail.com>
2023-05-15 18:16:38 -0400

    remove node modules add dotenv

--- server.js
+++ server.js
@@ -13,21 +13,25 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+    ssl: true,
+  },
 });
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => { res.send('app is working'); });
+app.get('/', (req, res) => {
+  res.send('app is working');
+});
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
 app.put('/image', image.handleImage(db));
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res); });
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port ${process.env.PORT}`);
