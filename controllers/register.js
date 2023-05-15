a027759abb4f67c461d952d597c4c7dce9769f93
Steve St.Pierre <stephencstpierre@gmail.com>
2023-05-15 18:40:13 -0400

    test change for deploy

--- controllers/register.js
+++ controllers/register.js
@@ -2,30 +2,32 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
 
+  console.log('handleRegister', email, name, password);
+
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
 
-  db.transaction(trx => {
-    trx.insert({ hash, email })
+  db.transaction((trx) => {
+    trx
+      .insert({ hash, email })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
             email: loginEmail[0],
             name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
-            res.json(user[0])
-          })
+          .then((user) => {
+            res.json(user[0]);
+          });
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  })
-  .catch(error => res.status(400).json('Unable to register'));
+  }).catch((error) => res.status(400).json('Unable to register'));
 };
 
 module.exports = { handleRegister };
